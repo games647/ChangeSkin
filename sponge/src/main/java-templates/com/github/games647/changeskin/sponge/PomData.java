e553bb73caee39abab6cc0ef48d085dd14948fb3
games647 <games647@users.noreply.github.com>
2018-02-22 17:27:02 +0100

    Add commit id to the version

--- sponge/src/main/java-templates/com/github/games647/changeskin/sponge/PomData.java
+++ sponge/src/main/java-templates/com/github/games647/changeskin/sponge/PomData.java
@@ -4,7 +4,7 @@ public class PomData {
 
     public static final String ARTIFACT_ID = "${project.parent.artifactId}";
     public static final String NAME = "${project.parent.name}";
-    public static final String VERSION = "${project.parent.version}";
+    public static final String VERSION = "${project.parent.version}-${git.commit.id.abbrev}";
     public static final String URL = "${project.parent.url}";
     public static final String DESCRIPTION = "${project.parent.description}";
 }
