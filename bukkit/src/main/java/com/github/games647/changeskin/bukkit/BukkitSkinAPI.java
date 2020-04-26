57e82a7fb9339e97999e7659678b9c5d8eec6889
OmerBenGera <omerthepro126@gmail.com>
2020-04-26 16:48:10 +0300

    Added an event for bukkit when changing skins

--- bukkit/src/main/java/com/github/games647/changeskin/bukkit/BukkitSkinAPI.java
+++ bukkit/src/main/java/com/github/games647/changeskin/bukkit/BukkitSkinAPI.java
@@ -2,6 +2,7 @@ package com.github.games647.changeskin.bukkit;
 
 import com.comphenix.protocol.wrappers.WrappedGameProfile;
 import com.comphenix.protocol.wrappers.WrappedSignedProperty;
+import com.github.games647.changeskin.bukkit.events.PlayerChangeSkinEvent;
 import com.github.games647.changeskin.bukkit.task.SkinApplier;
 import com.github.games647.changeskin.core.model.skin.SkinModel;
 import com.github.games647.changeskin.core.model.skin.SkinProperty;
@@ -10,6 +11,7 @@ import com.github.games647.changeskin.core.shared.ChangeSkinAPI;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class BukkitSkinAPI implements ChangeSkinAPI<Player, WrappedGameProfile> {
@@ -22,6 +24,8 @@ public class BukkitSkinAPI implements ChangeSkinAPI<Player, WrappedGameProfile> 
 
     @Override
     public void applySkin(Player receiver, SkinModel targetSkin) {
+        //Calling the event for changing skins
+        Bukkit.getPluginManager().callEvent(new PlayerChangeSkinEvent(receiver, targetSkin));
         WrappedGameProfile gameProfile = WrappedGameProfile.fromPlayer(receiver);
         applyProperties(gameProfile, targetSkin);
     }
